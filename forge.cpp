72c7bb46f928f56bb0a48e3e63f74e69b21773ea
John Wyman <52338900+JWyman482@users.noreply.github.com>
2022-10-18 16:31:23 -0700

    Create forge.cpp

---
+++ forge.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string auth = "YY"
+  cout << " -- ___ --\n//- \\|/ -\\\\\n|-  |_|  -|\n\\\\--|_|--//\n ~~~~~~~~~\nBy: " << auth << endl;
+}
